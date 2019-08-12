546c6d37b58e10222ae46eefafb24d49acd2431f
Brandon <brandon1221t@gmail.com>
2019-08-12 14:35:49 +0800

    Create a conditional <p> in case no existing orders.

--- src/Containers/Orders/Orders.js
+++ src/Containers/Orders/Orders.js
@@ -4,20 +4,24 @@ import Order from '../../Components/Order/Order';
 import withErrorHandler from '../../hoc/withErrorHandler';
 class Orders extends Component{
     state = {
-        orders: [],
+        orders: null,
         loading: true,
     }
     componentDidMount(){
         axios.get('/orders.json')
             .then(res => {
-                const fetchedOrders =[];
-                for (let key in res.data){
-                    fetchedOrders.push({
-                        ...res.data[key],
-                        id:key
-                    })
+                if (res.data) {
+                    const fetchedOrders =[];
+                    for (let key in res.data){
+                        fetchedOrders.push({
+                            ...res.data[key],
+                            id:key
+                        })
+                    }
+                    this.setState({loading:false, orders: fetchedOrders});
+                } else {
+                    this.setState({loading:false});
                 }
-                this.setState({loading:false, orders: fetchedOrders});
             })
             .catch(error => {
                 this.setState({loading:false});
@@ -25,24 +29,36 @@ class Orders extends Component{
     }
 
     render(){
-        // let orderList = (
-        //     this.state.orders.map(order => (
-        //         <Order 
-        //             key={order.id}
-        //             ingredients = {order.ingredients}
-        //             price = {(+order.price).toFixed(1)}
-        //             />   
-        //     ))
-        // )
+        let orderList = (
+            <div style = {{textAlign: 'center'}}>
+                <p>No Existing Orders.</p>
+                <p>Let's build own burger in Burger Builder Page!</p>
+            </div>
+        );
+
+        if (this.state.orders) {
+            orderList = (
+                this.state.orders.map(order => (
+                    <Order 
+                        key={order.id}
+                        ingredients = {order.ingredients}
+                        price = {(+order.price).toFixed(1)}
+                        />   
+                ))
+            )    
+        };
+        
         return(
             <div>
-                {this.state.orders.map(order => (
+                {orderList}
+                
+                {/* {this.state.orders.map(order => (
                     <Order 
                         key={order.id}
                         ingredients = {order.ingredients}
                         price = {(+order.price).toFixed(1)}
                     />   
-                ))}
+                ))} */}
             </div>
             );
     }
